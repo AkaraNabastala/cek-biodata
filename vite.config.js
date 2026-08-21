89deddf5d0087c3f03b2c588cd8655cb3448aca4
AkaraNabastala <kaneqken@gmail.com>
2026-08-21 22:20:45 +0700

    fix: atur base path otomatis untuk lokal dan github pages

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/./',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/cek-biodata/',
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.js',
+    }
   }
 })
